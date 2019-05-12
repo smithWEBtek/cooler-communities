a35132844c12ab9a300e3daa60ff37ff04c82b42
smithwebtek <brad@smithwebtek.com>
2019-05-12 19:44:06 -0400

    WIP 190512-743PM

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,6 +1,18 @@
 class ResponsesController < ApplicationController
 
   def create
-    render json: params
-  end
+  #   responses = []
+  #   params["data"]keys.each do |key|
+
+  #     question = Question.find_by_question_key(key)
+  #     new_reponse = {
+  #       user_id: current_user.id,
+  #       question_id: question.id,
+  #       answer: params["data"][key],
+  #       points: question.points
+  #     }
+  #     responses.push(new_reponse)
+  #   end
+  #   render json: responses
+  # end
 end
