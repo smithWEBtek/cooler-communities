79bccf245888035d506b88e4e17587176027cfd1
Brad Smith <brad@smithwebtek.com>
2019-05-13 09:04:14 -0400

    Reconcile question import for airtable & Surveyjs

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,14 +4,17 @@ class ResponsesController < ApplicationController
     @responses = []
     params[:data].keys.each_with_index do |question_key, index|
       question = Question.find_by(question_key: question_key)
-      answer = params[:data][question_key].downcase if params[:data][question_key]
+      user_answer = params[:data][question_key].downcase
+      binding.pry
+
       if question.answer_key.include?(params[:data][question_key])
         points = question.answer_key.include?(params[:data][question_key])
       end
+
       response_data = {
         user_id: current_user.id,
         question_id: question.id,
-        answer: answer,
+        answer: user_answer,
         points: points
       }
       new_response = Response.create(response_data)
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,30 +6,32 @@ class Question < ApplicationRecord
   belongs_to :survey
   has_many :responses
 
+  def self.build_answer_key(data)
+    answer_key = {}
+    data.each.with_index(5) do |row, index|
+      key = index % 2 == 0 ? index + 1 : index
+      question = data[key].nil? ? "blank" : data[key]
+      # points = data[key + 1].to_i || data[1].to_i
+      points = index * data[1].to_i
+      answer_key[question] = points
+    end
+    answer_key
+  end
+  
 	def self.import #import airtable_cooler.csv
     csv_text = File.read(Rails.root.join('lib', 'assets', 'airtable_cooler.csv'))
-   csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-   
-   csv.each do |row|
-    answer = {
-      "#{row[5]}": "#{row[6]}",
-      "#{row[7]}": "#{row[8]}",
-      "#{row[9]}": "#{row[10]}",
-      "#{row[11]}": "#{row[12]}",
-      "#{row[13]}": "#{row[14]}",
-      "#{row[15]}": "#{row[16]}"
-    }
-
-    question_data = {
-      survey_id: 1,
-      question_key: row[0],
-      airtable_id: row[1],
-      category_id: Category.find_or_create_by(title: row[2]).id,
-      question_text: row[3],
-      question_type: row[4], 
-      answer_key: answer
-    }
-
+    csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+    
+    csv.each do |row|
+      question_data = {
+        survey_id: 1,
+        question_key: row[0],
+        airtable_id: row[1],
+        category_id: Category.find_or_create_by(title: row[2]).id,
+        question_text: row[3],
+        question_type: row[4], 
+        answer_key: self.build_answer_key(row)
+      }
     new_question = Question.new(question_data)
       if new_question.save
         puts "."
