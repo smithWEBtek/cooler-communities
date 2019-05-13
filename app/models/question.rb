efc042d47ae121bec7063c5828da6a8a2c136a58
Brad Smith <brad@smithwebtek.com>
2019-05-13 18:01:54 -0400

    Generate points.json, move .json files to public

--- app/models/question.rb
+++ app/models/question.rb
@@ -21,7 +21,8 @@ class Question < ApplicationRecord
 	def self.import #import airtable_cooler.csv
     csv_text = File.read(Rails.root.join('lib', 'assets', 'airtable_cooler.csv'))
     csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-    
+    points = {}
+
     csv.each do |row|
       question_data = {
         survey_id: 1,
@@ -32,12 +33,28 @@ class Question < ApplicationRecord
         question_type: row[4], 
         answer_key: self.build_answer_key(row)
       }
-    new_question = Question.new(question_data)
+      points[row[0]] = self.build_answer_key(row)
+      new_question = Question.new(question_data)
+      
       if new_question.save
         puts "."
       else
         raise 'question NOT saved, please check airtable-cooler.csv for data accuracy'
       end
     end
+    self.clean_points(points)
   end
+  
+  def self.clean_points(points)
+    points_hash = {}
+    question_keys = points.keys
+    question_keys.each do |qkey|
+      points_hash[qkey] = {}
+      answer_keys = points[qkey].keys
+        answer_keys.each do |akey|
+          points_hash[qkey][akey.downcase] = points[qkey][akey]
+        end
+      end      
+      IO.write "public/assets/json/points.json", JSON.pretty_generate(points_hash)
+    end
 end
