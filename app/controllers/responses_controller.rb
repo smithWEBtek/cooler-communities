ba5a6a565e340e20d22059b1ff4c924b8755e2d9
smithwebtek <brad@smithwebtek.com>
2019-05-12 23:24:45 -0400

    Count and display points for current_user choices

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,18 +1,22 @@
 class ResponsesController < ApplicationController
 
   def create
-  #   responses = []
-  #   params["data"]keys.each do |key|
-
-  #     question = Question.find_by_question_key(key)
-  #     new_reponse = {
-  #       user_id: current_user.id,
-  #       question_id: question.id,
-  #       answer: params["data"][key],
-  #       points: question.points
-  #     }
-  #     responses.push(new_reponse)
-  #   end
-  #   render json: responses
-  # end
+    @responses = []
+    params[:data].keys.each_with_index do |question_key, index|
+      question = Question.find_by(question_key: question_key)
+      answer = params[:data][question_key].downcase if params[:data][question_key]
+      if question.answer_key.include?(params[:data][question_key])
+        points = question.answer_key.include?(params[:data][question_key])
+      end
+      response_data = {
+        user_id: current_user.id,
+        question_id: question.id,
+        answer: answer,
+        points: points
+      }
+      new_response = Response.create(response_data)
+      @responses.push(new_response)
+    end
+    render json: @responses
+  end
 end
