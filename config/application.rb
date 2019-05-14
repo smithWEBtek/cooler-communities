fa5d8ca62c62df57990a6c6f95a8db8df590392d
Brad Smith <brad@smithwebtek.com>
2019-05-14 15:30:06 -0400

    WIP 190514-329pm

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Cooler
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.assets.paths << Rails.root.join('app', 'assets', 'webapp')
+
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
